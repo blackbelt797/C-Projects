0189f9251a3cf1c875ff67ba424578a9e6215210
Gregg Cline <ggcline82@gmail.com>
2024-01-29 07:13:08 -0800

    Set up to practice C++ Workbook

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+//Start of  your program 
+
+int main()
+{
+    //tell the compiler what nameapace to search in using namespace std;
+    using namespace std;
+  
+   
+
+    //Return a Value to the OS
+    return 0;
+
+}
